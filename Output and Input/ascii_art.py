6435280d290d549055aef79853bf4d5ba3ab222f
lyanW-M <lyanw@wsc.school.nz>
2026-05-05 11:26:49 +1200

    Create ascii_art.py

---
+++ Output and Input/ascii_art.py
@@ -0,0 +1,16 @@
+### ASCII Art ###
+# Create a picture using lines, spaces and/or some letters.
+# You'll need to use print multiple times to create your picture.
+
+print("""   _______
+ /          \\
+|    O   O    |
+|       V      |
+ \\_______/
+""")
+
+
+# -------------------------
+
+# EXTENSION
+# Try to create a more complicated ASCII picture
