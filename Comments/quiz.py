eafa70f20907579a95caee551831891648557f93
lyanW-M <lyanw@wsc.school.nz>
2026-05-15 12:33:53 +1200

    Update quiz.py

--- Comments/quiz.py
+++ Comments/quiz.py
@@ -2,10 +2,14 @@
 print("WELCOME TO THE QUIZ SHOW!")
 print("Answer these 5 questions in order to win.... NOTHING!")
 
+#Score
+score = int(0)
+
 #Question 1
 q1 = input("First question; How do you spell cat?\n")
 if q1 in ["cat", "CAT", "Cat"]:
     print("CORRECT")
+    score += 1
 else:
     print("WRONG!!!")
 
@@ -13,6 +17,7 @@ else:
 q2 = input("What is the capital of India?\n")
 if q2 in ["New Delhi", "NEW DELHI", "new delhi"]:
     print("CORRECT")
+    score += 1
 else:
     print("WRONG!!!")
 
@@ -20,6 +25,7 @@ else:
 q3 = input("What is 1*1?\n")
 if q3 in ["1", "one", "One", "ONE"]:
     print("CORRECT")
+    score += 1
 else:
     print("WRONG!!!")
 
@@ -27,6 +33,7 @@ else:
 q4 = input("Winner says what?\n")
 if q4 in ["What", "what", "WHAT"]:
     print("CORRECT")
+    score += 1  
 else:
     print("WRONG!!!")
 
@@ -34,5 +41,14 @@ else:
 q5 = input("Is Nivedh aura?\n")
 if q5 in ["No", "no", "NO", "nah", "Nah", "NAH"]:
     print("CORRECT")
+    score += 1
 else:
     print("WRONG!!!")
+
+#Final Score
+print("Your final score is ", score, "/5")
+
+if score == 5:
+    print("Perfect score, 5/5")
+else:
+    print("You're score is ", score, "out of 5, better luck next time!")
